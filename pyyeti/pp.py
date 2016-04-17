2b81411c11955234c1483bf38164886e86e625c0
Tim Widrick <twmacro@gmail.com>
2016-04-17 10:43:35 -0400

    add class printing to pp.PP, v0.83.7

--- pyyeti/pp.py
+++ pyyeti/pp.py
@@ -8,10 +8,10 @@ from types import SimpleNamespace
 
 class PP:
     """
-    A very simple hack for pretty printing data structures.
+    A simple class for pretty printing data structures.
     """
     def __init__(self, var=None, depth=5, tab=4,
-                 keylen=40, strlen=80):
+                 keylen=40, strlen=80, show_hidden=False):
         """
         Initializer for `PP`.
 
@@ -27,6 +27,10 @@ class PP:
             Maximum length for dictionary (and similar) keys.
         strlen : integer; optional
             Maximum length for dictionary (and similar) values.
+        show_hidden : bool; optional
+            For classes, if `show_hidden` is True, show
+            members that start with '_'. Default is to not
+            show them.
 
         Notes
         -----
@@ -84,14 +88,41 @@ class PP:
             'r'          : int16 ndarray 4 elems: (4,) [0 1 2 3]
         <BLANKLINE>
         <...>
+
+        Classes have the `show_hidden` option:
+
+        >>> class A:
+        ...     a = 9
+        ...     b = {'variable': [1, 2, 3]}
+        >>> PP(A)       # doctest: +ELLIPSIS
+        class[n=6]
+            .a: 9
+            .b: dict[n=1]
+                'variable': [1, 2, 3]
+        <BLANKLINE>
+        <...>
+
+        >>> PP(A, show_hidden=1)       # doctest: +ELLIPSIS
+        class[n=6]
+            .__dict__   : <attribute '__dict__' of 'A' objects>
+            .__doc__    : None
+            .__module__ : 'pyyeti.pp'
+            .__weakref__: <attribute '__weakref__' of 'A' objects>
+            .a          : 9
+            .b          : dict[n=1]
+                'variable': [1, 2, 3]
+        <BLANKLINE>
+        <...>
         """
         self._tab = tab
         self._depth = depth
         self._keylen = keylen
         self._strlen = strlen
+        self._show_hidden = show_hidden
         self._functions = {
             dict: self._dict_string,
             SimpleNamespace: self._ns_string,
+            type: self._class_string,
             np.ndarray: self._array_string,
             h5py._hl.dataset.Dataset: self._h5data_string,
             }
@@ -101,9 +132,9 @@ class PP:
     def _lead_string(self, level):
         self.output = self.output + ' '*self._tab*level
 
-    def _key_string(self, val, typename):
+    def _key_string(self, val, isns):
         if isinstance(val, str):
-            if typename == 'namespace':
+            if isns:
                 s = "." + val
             else:
                 s = "'" + val + "'"
@@ -146,11 +177,19 @@ class PP:
             s = s + self._getarrstr(arr[...])
         self.output = self.output + s + '\n'
 
-    def _dict_string(self, dct, level, typename='dict'):
+    def _get_keys(self, dct, showhidden):
+        if not showhidden:
+            keys = [k for k in dct if k[0] != '_']
+        else:
+            keys = list(dct.keys())
+        return keys
+
+    def _dict_string(self, dct, level, typename='dict', isns=False,
+                     showhidden=True):
         self.output = (self.output + '{}[n={}]\n'
                        .format(typename, len(dct)))
         if level < self._depth:
-            keys = list(dct)
+            keys = self._get_keys(dct, showhidden)  # list(dct)
             try:
                 keys = sorted(keys)
             except TypeError:
@@ -159,16 +198,22 @@ class PP:
             # get max key length for pretty printing:
             n = 0
             for k in keys:
-                n = max(n, len(self._key_string(k, typename)))
+                n = max(n, len(self._key_string(k, isns)))
             frm = '{:<' + str(n) + 's}: '
             for k in keys:
                 self._lead_string(level)
-                s = self._key_string(k, typename)
+                s = self._key_string(k, isns)
                 self.output = self.output + frm.format(s)
                 self._print_var(dct[k], level)
 
     def _ns_string(self, ns, level):
-        self._dict_string(ns.__dict__, level, typename='namespace')
+        self._dict_string(ns.__dict__, level,
+                          typename='namespace', isns=True)
+
+    def _class_string(self, ns, level):
+        self._dict_string(ns.__dict__, level,
+                          typename='class', isns=True,
+                          showhidden=self._show_hidden)
 
     def _print_var(self, var, level):
         try:
