5e1851a2f6fa3923c9543450e97274fe7d3b87c2
Tim Widrick <twmacro@gmail.com>
2025-02-24 12:38:11 -0500

    tag version 1.4.3.5

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ def run_setup(with_binary):
     install_requires = check_dependencies()
     setup(
         name="pyyeti",
-        version="1.4.3.4",
+        version="1.4.3.5",
         url="http://github.com/twmacro/pyyeti/",
         license="BSD",
         author="Tim Widrick",
